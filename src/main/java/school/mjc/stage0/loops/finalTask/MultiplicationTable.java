4dddf263825249143fea7618853414a3dafe35fa
Your Name <you@example.com>
2023-12-24 23:07:33 +0500

    thank you

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
-    public void printTable(int numberTableToPrint){
+
+    public static int printTable(int numberTableToPrint) {
+        int limit = 10;
+
+        for (int i = 1; i <= limit; i++) {
+            int multiplicationResult = numberTableToPrint * i;
+            System.out.println(i + " x " + numberTableToPrint + " = " + multiplicationResult);
+
+        }
+        return numberTableToPrint;
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -1,11 +1,14 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class Power {
-    public void printPower(int numberToPrint, int power) {
 
-    }
+    public  void printPower(int numberToPrint, int power) {
+        long result = 1;
 
-    public static void main(String[] args) {
-        new Power().printPower(10, 3);
+        for (int i = 0; i < power; i++) {
+            result *= numberToPrint;
+        }
+
+        System.out.println(result);
     }
 }
